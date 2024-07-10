50b90859f8f1eecc1bc861c37cf3ad2ffac761a1
superbryanlee <bryanlee.fh@gmail.com>
2024-07-09 17:29:29 -0700

    More logic for photos.

--- src/PhotographySection.tsx
+++ src/PhotographySection.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
 export const PhotographySection = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <section className='photography-section'>
-      <div className='text-section-margin-bottom'>
+      <div>
         I'm a casual cinephile and also <strong>love</strong> film and digital
-        photography. Here are some of my favourite <strong>frames</strong>...
+        photography. Here are some of my favourite{" "}
+        <span
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          className='my-frames'
+        >
+          frames...
+        </span>
       </div>
-      <img src='public/photography/000914930036.jpg'></img>
+      {isOpen ? (
+        <>
+          <img
+            className='frame'
+            src='public/photography/000914930036.jpg'
+          ></img>
+          <img
+            className='frame'
+            src='public/photography/000816370009.jpg'
+          ></img>
+        </>
+      ) : null}
     </section>
   );
 };
