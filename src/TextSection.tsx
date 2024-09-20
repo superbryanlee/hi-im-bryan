c528e22e287c00b6d45abcd2a12f6f46e6ce653b
superbryanlee <bryanlee.fh@gmail.com>
2024-09-20 15:15:31 -0700

    Changes.

--- src/TextSection.tsx
+++ src/TextSection.tsx
@@ -2,18 +2,19 @@ export const TextSection = () => {
   return (
     <section className='text-box'>
       <div className='text-section-margin-top seeking'>
-        I'm seeking <strong>2025</strong> new grad opportunities!{" "}
+        I'm seeking <strong>2025</strong> new grad & internship opportunities!{" "}
         <a href='https://www.linkedin.com/in/bryanleeubc/'>Let's chat</a>.
       </div>
       <div className='text-section-margin-top currently'>
-        <strong>Currently</strong>, I’m doing a summer internship placement as a
-        Backend Engineer at <a href='https://www.faire.com/'>Faire</a> before I
-        enter my final year of Computer Science at{" "}
-        <a href='https://www.ubc.ca/'>UBC</a>.
+        <strong>Currently</strong>, I'm wrapping up my final year of Computer
+        Science at <a href='https://www.ubc.ca/'>UBC</a>.
       </div>
       <div className='text-section-margin-top previously'>
         <strong>Previously</strong>, I’ve done internships as a:
         <ul className='list-positions'>
+          <li>
+            Backend Engineer at <a href='https://www.faire.com/'>Faire</a>
+          </li>
           <li>
             Frontend Engineer at <a href='https://www.faire.com/'>Faire</a>
           </li>
