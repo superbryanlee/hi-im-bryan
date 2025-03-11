c5746fd2ba1d7b57755b79355fa8365709302bbf
superbryanlee <bryanlee.fh@gmail.com>
2025-03-11 11:05:24 -0700

    Incorporated memoization.

--- src/PhotographySection.tsx
+++ src/PhotographySection.tsx
@@ -1,41 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
+
 export const PhotographySection = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  const imageSources = useMemo(
+    () => [
+      "photography/000914930036.jpg",
+      "photography/000816370009.jpg",
+      "photography/0014.jpg",
+      "photography/2024-06-05_16-00-42_525.jpeg",
+      "photography/2024-06-18_13-43-08_987.jpeg",
+      "photography/2024-06-18_13-43-13_627.jpeg",
+      "photography/DSCF3383.JPG",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const preloadImages = async () => {
+      await Promise.all(
+        imageSources.map((src) => {
+          return new Promise((resolve) => {
+            const img = new Image();
+            img.src = src;
+            img.onload = resolve;
+            img.onerror = resolve; // Prevents blocking on error
+          });
+        })
+      );
+      setImagesLoaded(true);
+    };
+
+    preloadImages();
+  }, [imageSources]); // imageSources is now stable
 
   return (
     <section className='photography-section'>
       <div>
         I'm a casual cinephile and also <strong>love</strong> film and digital
         photography. Here are some{" "}
-        <span
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-          className='my-frames'
-        >
+        <span onClick={() => setIsOpen(!isOpen)} className='my-frames'>
           photos I've taken...
         </span>
       </div>
-      {isOpen ? (
+      {isOpen && imagesLoaded && (
         <>
-          <img className='frame' src='photography/000914930036.jpg'></img>
-          <img className='frame' src='photography/000816370009.jpg'></img>
-          <img className='frame' src='photography/0014.jpg'></img>
-          <img
-            className='frame'
-            src='photography/2024-06-05_16-00-42_525.jpeg'
-          ></img>
-          <img
-            className='frame'
-            src='photography/2024-06-18_13-43-08_987.jpeg'
-          ></img>
-          <img
-            className='frame'
-            src='photography/2024-06-18_13-43-13_627.jpeg'
-          ></img>
-          <img className='frame' src='photography/DSCF3383.JPG'></img>
+          {imageSources.map((src, index) => (
+            <img key={index} className='frame' src={src} />
+          ))}
         </>
-      ) : null}
+      )}
     </section>
   );
 };
